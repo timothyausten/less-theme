3ef12dc3576f37bcd71e8301e44dc255e01c01bc
Timothy Austen <dfdfdfcvcvcv@gmail.com>
2016-07-06 17:40:12 +0300

    Added ability to cycle through color themes by clicking next.

---
+++ less-themes.js
@@ -0,0 +1,55 @@
+var themes = {};
+
+themes.grayaccented = {
+    '@headerbackground': '#999',
+    '@headeraccent': '#f0f',
+    '@footerbackground': '#fff',
+    '@footertext': '#777',
+    '@contentbackground': 'hsla(0, 100%, 50%, 0)',
+    '@footertopbordercolor': 'hsla(0, 0, 0, .2)',
+    '@contenttext': '#777',
+    '@contentheadertext': '#666',
+    '@sidebarbackground': '#bbb',
+    '@nav-list-item-a': '#bbb, #aaa, #999',
+    '@nav-list-item-b': '#aaa, #999, #777',
+    '@nav-list-item1': '@nav-list-item-a',
+    '@nav-list-item2': '@nav-list-item-a',
+    '@nav-list-item3': '@nav-list-item-a',
+    '@nav-list-item4': '@nav-list-item-a',
+    '@nav-list-item5': '@nav-list-item-a'
+};
+themes.grayaccentedmore = {
+    '@headerbackground': '#999',
+    '@headeraccent': '#f0f',
+    '@footerbackground': '#fff',
+    '@footertext': '#777',
+    '@contentbackground': 'hsla(0, 100%, 50%, 0)',
+    '@footertopbordercolor': 'hsla(0, 0, 0, .2)',
+    '@contenttext': '#777',
+    '@contentheadertext': '#666',
+    '@sidebarbackground': '#bbb',
+    '@nav-list-item-a': '#bbb, #aaa, #999',
+    '@nav-list-item-b': '#aaa, #999, #777',
+    '@nav-list-item1': '#fac5c5, #ff6a6a, #ff3434',
+    '@nav-list-item2': '#a0dda0, #55db55, #00b900',
+    '@nav-list-item3': '#7ab3e4, #2d8de0, #0065bc',
+    '@nav-list-item4': '#81c8b5, #43cca7, #00986f',
+    '@nav-list-item5': '#cb9de0, #c478e6, #a932de'
+};
+themes.saturated = {
+    '@headerbackground': '#a3c6e4',
+    '@headeraccent': '#fff500',
+    '@footerbackground': 'hsla(0, 0, 0, 0%)',
+    '@footertext': '#fff',
+    '@contentbackground': 'hsla(0, 100%, 74%, 100%)',
+    '@footertopbordercolor': 'hsla(100%, 100%, 100%, .5)',
+    '@contentheadertext': '#fff',
+    '@contenttext': '#fff',
+    '@sidebarbackground': '#fac5c5',
+    '@nav-list-item1': '#fac5c5, #ff6a6a, #ff3434',
+    '@nav-list-item2': '#a0dda0, #55db55, #00b900',
+    '@nav-list-item3': '#7ab3e4, #2d8de0, #0065bc',
+    '@nav-list-item4': '#81c8b5, #43cca7, #00986f',
+    '@nav-list-item5': '#cb9de0, #c478e6, #a932de'
+};
+
--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 
-// Step 2: Set development mode to see errors
+
+var a, current, testclick, themesAsArray = {};
+
+// Set development mode to see errors
 less.env = 'development';
+
+
+// Make array out of themes object in order to cycle through themes.
+themesAsArray = Object.keys(themes).map(
+    function (key) {
+        return themes[key];
+    }
+);
+
+current = 0;
+
+function themeNext(increment) {
+    'use strict';
+    less.modifyVars(themesAsArray[increment + 1]);
+    current = increment === themesAsArray.length - 1 ? 0 : increment + 1;
+}
+
+function themeNextRolled() {
+    themeNext(current);
+}
+
+testclick = document.getElementById('footer');
+testclick.onclick = themeNextRolled;
+
