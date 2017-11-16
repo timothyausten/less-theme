014028d5f3ac9d38611452db589584b95d1315b1
Timothy Austen <dfdfdfcvcvcv@gmail.com>
2017-11-16 14:05:40 +0200

    Make button amber colored

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
 
+// Click buttons to open and close nav menu
+(function () {
+    var sidebarOuter, openbtn, closebtn;
+    sidebarOuter = document.getElementsByClassName('sidebar-outer')[0];
+    openbtn = document.getElementsByClassName('openbtn')[0];
+    closebtn = document.getElementsByClassName('closebtn')[0];
+    openbtn.onclick = function () {
+        sidebarOuter.style.left = '0';
+    };
+    closebtn.onclick = function () {
+        sidebarOuter.style.left = '-8em';
+    };
+}());
 
-var a, current, testclick, themesAsArray = {};
 
-// Set development mode to see errors
-less.env = 'development';
-
-
-// Make array out of themes object in order to cycle through themes.
-themesAsArray = Object.keys(themes).map(
-    function (key) {
-        return themes[key];
+// Cycle through themes using LESS CSS
+(function () {
+    var a, current, testclick, themesAsArray = {};
+    // Set development mode to see errors
+    less.env = 'development';
+    // Make array out of themes object in order to cycle through themes.
+    themesAsArray = Object.keys(themes).map(
+        function (key) {
+            return themes[key];
+        }
+    );
+    current = 0;
+    function themeNext(increment) {
+        'use strict';
+        // Modify LESS variables using next color theme
+        less.modifyVars(themesAsArray[increment + 1]);
+        // If current theme is last in list, go back to first in list
+        current = increment === themesAsArray.length - 1 ? 0 : increment + 1;
     }
-);
-
-current = 0;
-
-function themeNext(increment) {
-    'use strict';
-    less.modifyVars(themesAsArray[increment + 1]);
-    current = increment === themesAsArray.length - 1 ? 0 : increment + 1;
-}
-
-function themeNextRolled() {
-    themeNext(current);
-}
-
-testclick = document.getElementById('footer');
-testclick.onclick = themeNextRolled;
-
+    function themeNextRolled() {
+        themeNext(current);
+    }
+    testclick = document.getElementsByClassName('theme-btn')[0];
+    testclick.onclick = themeNextRolled;
+}());
